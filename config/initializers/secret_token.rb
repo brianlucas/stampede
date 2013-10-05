791f1e1ef72a64530165afb4225cf62fb9b89fca
Lukasz Wilczak <lukasz.wilczak@repo.wszyscyklikamy.pl>
2013-10-06 00:04:04 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stampede::Application.config.secret_key_base = 'cf129030f11a4aad014154f0b9c7d89b28c8162654bcb6e2e6137f02f696cb9bb23ca6b92437137f8aedbadbea07068e3a8b99b50575ec908d690e2ae87bc8d2'
